4f3c35602241c8e9477960d0186f5a20631d07e8
Marc O'Morain <github.com@marcomorain.com>
2013-12-29 17:13:42 +0000

    Make the game start up

--- WinQuake/gl_vidglfw.c
+++ WinQuake/gl_vidglfw.c
@@ -27,6 +27,8 @@ void D_EndDirectRect (int x, int y, int width, int height)
 
 void VID_Shutdown(void)
 {
+    Con_Print("Shutting down GLFW");
+    glfwTerminate();
 }
 
 void InitSig(void)
@@ -71,4 +73,49 @@ void VID_Init8bitPalette(void)
 
 void VID_Init(unsigned char *palette)
 {
+    vid.maxwarpwidth = 320; // WARP_WIDTH;
+	vid.maxwarpheight = 200; // WARP_HEIGHT;
+	vid.colormap = host_colormap;
+	vid.fullbright = 256 - LittleLong (*((int *)vid.colormap + 2048));
+    vid.width = 320;
+    vid.height = 200;
+    
+    
+    /* Initialize the library */
+    if (!glfwInit()) {
+        Sys_Error("Error initialising GLFW");
+    }
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(vid.width, vid.height, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        Sys_Error("Error creating GLFW window");
+    }
+    
+    Con_Printf("GLFW initialised %s", glfwGetVersionString());
+    
+    glfwMakeContextCurrent(window);
+    
+    // Copied GL code from Linux
+    glClearColor (1,0,0,0);
+	glCullFace(GL_FRONT);
+	glEnable(GL_TEXTURE_2D);
+    
+	glEnable(GL_ALPHA_TEST);
+	glAlphaFunc(GL_GREATER, 0.666);
+    
+	glPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
+	glShadeModel (GL_FLAT);
+    
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+	glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    //	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 }
