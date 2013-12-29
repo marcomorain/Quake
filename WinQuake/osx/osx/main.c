dea27fceb2aff75bbb2e6875a7c19dcce56764b4
Marc O'Morain <github.com@marcomorain.com>
2013-12-29 12:45:09 +0000

    Create empty xcode project

---
+++ WinQuake/osx/osx/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  osx
+//
+//  Created by Marc O'Morain on 29/12/2013.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
