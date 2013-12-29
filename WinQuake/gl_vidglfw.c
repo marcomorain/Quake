61a1586b113586c8a5d428da15323b30a5543029
Marc O'Morain <github.com@marcomorain.com>
2013-12-29 16:00:05 +0000

    Make the program link and run

---
+++ WinQuake/gl_vidglfw.c
@@ -0,0 +1,72 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <signal.h>
+#include "quakedef.h"
+
+float gldepthmin, gldepthmax;
+int texture_extension_number = 1;
+int		texture_mode = GL_LINEAR;
+const char *gl_renderer = "none";
+qboolean isPermedia = false;
+qboolean gl_mtexable = false;
+cvar_t	gl_ztrick = {"gl_ztrick","1"};
+
+unsigned		d_8to24table[256];
+unsigned char	d_15to8table[65536];
+
+
+void D_BeginDirectRect (int x, int y, byte *pbitmap, int width, int height)
+{
+}
+
+void D_EndDirectRect (int x, int y, int width, int height)
+{
+}
+
+void VID_Shutdown(void)
+{
+}
+
+void InitSig(void)
+{
+}
+
+void VID_ShiftPalette(unsigned char *p)
+{
+//	VID_SetPalette(p);
+}
+
+void	VID_SetPalette (unsigned char *palette)
+{
+}
+
+void GL_Init (void)
+{
+}
+
+void GL_BeginRendering (int *x, int *y, int *width, int *height)
+{
+}
+
+
+void GL_EndRendering (void)
+{
+}
+
+void Init_KBD(void)
+{
+}
+
+qboolean VID_Is8bit(void)
+{
+	//return is8bit;
+    return 1;
+}
+
+void VID_Init8bitPalette(void) 
+{
+}
+
+void VID_Init(unsigned char *palette)
+{
+}
