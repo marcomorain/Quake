3c598b5b138c1a8a2f4d439c8ae9f05f7d07984b
Marc O'Morain <github.com@marcomorain.com>
2014-06-01 18:12:41 +0100

    Create an empty project

---
+++ WinQuake/Quake/Quake/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Quake
+//
+//  Created by Marc O'Morain on 01/06/2014.
+//  Copyright (c) 2014 Quake. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
