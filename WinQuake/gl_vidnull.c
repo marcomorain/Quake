ef1d87ca2d133223177887440c879f8ac2f77cab
Marc O'Morain <github.com@marcomorain.com>
2014-06-21 16:19:12 +0100

    GLFW

--- WinQuake/gl_vidnull.c
+++ WinQuake/gl_vidnull.c
@@ -19,7 +19,21 @@
  */
 
 #include "quakedef.h"
+#include <GLFW/glfw3.h>
 
+static GLFWwindow* window = NULL;
+
+struct pos {
+  double x;
+  double y;
+};
+
+struct {
+  int width;
+  int height;
+  struct pos mouse_delta;
+  struct pos mouse;
+} window_settings;
 
 unsigned short	d_8to16table[256];
 unsigned	d_8to24table[256];
@@ -42,6 +56,8 @@ qboolean gl_mtexable = false;
 
 void VID_Shutdown(void)
 {
+    Con_SafePrintf("Shutting down GLFW\n");
+    glfwTerminate();
 }
 
 void VID_ShiftPalette(unsigned char *p)
@@ -90,6 +106,61 @@ void VID_Init8bitPalette(void)
 {
 }
 
+static void error_callback(int error, const char* description)
+{
+    Con_Printf("GLFW Error 0x%08x: %s\n", error, description);
+}
+
 void VID_Init(unsigned char *palette)
 {
+    Init_KBD();
+
+    memset(&window_settings, 0, sizeof(window_settings));
+    window_settings.height    = 240; //1024;
+    window_settings.width     = 320; // 1280;
+
+    glfwSetErrorCallback(error_callback);
+
+    /* Initialize the library */
+    if (!glfwInit()) {
+        Sys_Error("Error initialising GLFW");
+    }
+    vid.maxwarpwidth  = window_settings.width;  // WARP_WIDTH;
+	vid.maxwarpheight = window_settings.height; // WARP_HEIGHT;
+    vid.width  = window_settings.width;
+    vid.height = window_settings.height;
+
+	vid.colormap = host_colormap;
+	vid.fullbright = 256 - LittleLong (*((int *)vid.colormap + 2048));
+
+    vid.conwidth = 640;
+
+	vid.conwidth &= 0xfff8; // make it a multiple of eight
+
+	if (vid.conwidth < 320)
+		vid.conwidth = 320;
+
+	// pick a conheight that matches with correct aspect
+	vid.conheight = vid.conwidth*3 / 4;
+
+	if (vid.conheight < 200)
+		vid.conheight = 200;
+
+	if (vid.conheight > vid.height)
+		vid.conheight = vid.height;
+	if (vid.conwidth > vid.width)
+		vid.conwidth = vid.width;
+
+	vid.aspect = ((float)vid.height / (float)vid.width) * (320.0 / 240.0);
+	vid.numpages = 2;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(vid.width, vid.height, "Quake", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        Sys_Error("Error creating GLFW window");
+    }
+
+    Con_Printf("GLFW initialised %s\n", glfwGetVersionString());
 }
